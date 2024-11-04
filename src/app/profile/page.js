c059055463b80f2bc4f1075afc2dab17a21597c0
AndreiReznikov <andrezniksk@yandex.ru>
2024-11-04 18:23:08 +0800

    added sign in form

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -4,6 +4,18 @@ export default async function ProfilePage() {
   return (
     <div className={styles.container}>
       <h1>Profile</h1>
+      <div className={styles['form-container']}>
+        <form method='post' className={styles.form}>
+          <h2>Sign in</h2>
+          <div className={styles['inputs-wrapper']}>
+            <input className={styles.input} type='email' placeholder='Email' required />
+            <input className={styles.input} type='password' placeholder='Password' required />
+          </div>
+          <div className={styles['button-wrapper']}>
+            <button className={styles.button} type='submit'>Sign in</button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
