aab36c9877810fea6471f5a08ff986633af97a12
AndreiReznikov <andrezniksk@yandex.ru>
2024-11-30 13:30:44 +0800

    added subtitle shown condition

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
 import Link from 'next/link'
-import styles from './page.module.css'
 import { getServerSession } from 'next-auth'
+import { authOptions } from './api/auth/[...nextauth]/route'
+import styles from './page.module.css'
 
-export default function Home() {
-  const session = getServerSession();
+export default async function Home() {
+  const session = await getServerSession(authOptions);
 
   return (
     <main className={styles.main}>
@@ -11,9 +12,11 @@ export default function Home() {
         <h1 className={styles.title}>
           Hello! Welcome to the Next Blog! Here you can write your own posts, as well as read the posts of other authors.
         </h1>
-        <h2 className={styles.subtitle}>
-          To become our author, <Link className={styles.link} href='/login'>log in</Link> or <Link className={styles.link} href='/register'>register</Link>
-        </h2>
+        {!session?.user &&
+          <h2 className={styles.subtitle}>
+            To become our author, <Link className={styles.link} href='/login'>log in</Link> or <Link className={styles.link} href='/register'>register</Link>
+          </h2>
+        }
       </div>
     </main>
   )
